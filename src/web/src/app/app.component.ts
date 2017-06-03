93f491cf9fdca4129e1d440ef09e39394b9f99d7
zhangaz1 <zhangaz1@hotmail.com>
2017-06-03 18:14:07 +0800

    use page configs

--- src/web/src/app/app.component.ts
+++ src/web/src/app/app.component.ts
@@ -10,7 +10,39 @@ import {run} from './run';
 export class AppComponent {
     title = 'app works!';
 
+    configs = [];
+
     run() {
-		run();
+        this.configs = this.getConfigs();
+        let config = {
+			userName: 'zs',
+			// win: remote.getCurrentWindow(),
+			instanceConfigs: this.configs,
+		};
+
+		run(config);
     }
+
+    getConfigs() {
+        return [{
+			id: 1,
+			userName: 'abc@163.com',
+			password: 'abc123',
+			productUrl: 'http://www.adidas.com.cn/cg5804',
+			size: 42,
+			proxy: 'http://61.191.41.130:80',
+			proxyUserName: null,
+			proxyPassword: null,
+		}, {
+				id: 2,
+				userName: 'abc@163.com',
+				password: 'abc123',
+				productUrl: 'http://www.adidas.com.cn/cg5804',
+				size: 42,
+				proxy: 'http://61.191.41.130:80',
+				proxyUserName: null,
+				proxyPassword: null,
+			}];
+    }
+
 }
--- src/web/src/app/run.js
+++ src/web/src/app/run.js
@@ -1,29 +1,5 @@
 exports.run = run;
 
-function run() {
-	var config = {
-		userName: 'zs',
-		// win: remote.getCurrentWindow(),
-		instanceConfigs: [{
-			id: 1,
-			userName: 'abc@163.com',
-			password: 'abc123',
-			productUrl: 'http://www.adidas.com.cn/cg5804',
-			size: 42,
-			proxy: 'http://61.191.41.130:80',
-			proxyUserName: null,
-			proxyPassword: null,
-		}, {
-			id: 2,
-			userName: 'abc@163.com',
-			password: 'abc123',
-			productUrl: 'http://www.adidas.com.cn/cg5804',
-			size: 42,
-			proxy: 'http://61.191.41.130:80',
-			proxyUserName: null,
-			proxyPassword: null,
-		}],
-	};
-
+function run(config) {
 	electron.ipcRenderer.send('command', config);
 }
