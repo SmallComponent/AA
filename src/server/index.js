e14b73db76bb19858eab06e7cfd0f85852b67286
zhangaz1 <zhangaz1@hotmail.com>
2017-06-04 21:17:41 +0800

    add koa-mongo

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,21 @@
 const koaStatic = require('koa-static');
 const Koa = require('koa');
-const router = require('./router');
 const koaBody = require('koa-body');
-
+const koaMongo = require('koa-mongo');
 const app = new Koa();
+
+const router = require('./router');
+
+const mongoOption = {
+	// uri: 'mongodb://admin:123456@localhost:27017/test', //or url
+	uri: 'mongodb://localhost:27017/aa',
+	max: 100,
+	min: 1,
+};
 
 app
 	.use(koaBody())
+	.use(koaMongo(mongoOption))
 	.use(router.routes())
 	.use(router.allowedMethods())
 	.use(koaStatic('./static'));
