5e5320813023353014b8579da06269f8e84069ff
zhangaz1 <zhangaz1@hotmail.com>
2017-06-04 21:45:05 +0800

    convert id _id

--- src/server/modules/config/index.js
+++ src/server/modules/config/index.js
@@ -3,38 +3,68 @@ module.exports = {
 	saveConfigs,
 };
 
+const dbName = 'AA';
+const collectionName = 'configs';
 
 function getConfigs(ctx, next) {
-	ctx.body = {
-		data: configs,
-	};
+	return ctx.mongo
+		.db(dbName)
+		.collection(collectionName)
+		.find()
+		.toArray()
+		.then(configs => {
+			configs.forEach(config => {
+				config.id = config._id;
+				delete config._id;
+			});
+
+			ctx.body = {
+				data: configs,
+			};
+		});
+
+	// ctx.mongo.db('test').collection('users').remove({
+	// 	// _id: mongo.ObjectId(userId)
+	// });
 }
 
 function saveConfigs(ctx, next) {
-	let data = ctx.request.body.data;
-	configs = data;
-	ctx.body = {
-		data: true,
-	};
+	let configs = ctx.request.body.data;
+
+	configs.forEach(config => {
+		config._id = config.id;
+		delete config.id;
+	});
+
+
+	return ctx.mongo
+		.db(dbName)
+		.collection(collectionName)
+		.insert(configs)
+		.then(result => {
+			ctx.body = {
+				data: result.result.ok,
+			};
+		});
 }
 
-let configs = [{
-	id: 1,
-	userName: 'abc@163.com',
-	password: 'abc123',
-	productUrl: 'http://www.adidas.com.cn/cg5804',
-	size: 42,
-	proxy: 'http://61.191.41.130:80',
-	proxyUserName: '',
-	proxyPassword: '',
-	status: 'xxx',
-}, {
-	id: 2,
-	userName: 'abcd@163.com',
-	password: 'abcd163',
-	productUrl: 'http://www.adidas.com.cn/cg5804',
-	size: 42,
-	proxy: 'http://61.191.41.130:80',
-	proxyUserName: null,
-	proxyPassword: null,
-}, ];
+// let configs = [{
+// 	id: 1,
+// 	userName: 'abc@163.com',
+// 	password: 'abc123',
+// 	productUrl: 'http://www.adidas.com.cn/cg5804',
+// 	size: 42,
+// 	proxy: 'http://61.191.41.130:80',
+// 	proxyUserName: '',
+// 	proxyPassword: '',
+// 	status: 'xxx',
+// }, {
+// 	id: 2,
+// 	userName: 'abcd@163.com',
+// 	password: 'abcd163',
+// 	productUrl: 'http://www.adidas.com.cn/cg5804',
+// 	size: 42,
+// 	proxy: 'http://61.191.41.130:80',
+// 	proxyUserName: null,
+// 	proxyPassword: null,
+// }, ];
