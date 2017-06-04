f64a9e7384b4cafd3449cf0abee6851464817a65
zhangaz1 <zhangaz1@hotmail.com>
2017-06-04 10:57:40 +0800

    add import

--- src/web/src/app/components/import/import.component.ts
+++ src/web/src/app/components/import/import.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-import',
-  templateUrl: './import.component.html',
-  styleUrls: ['./import.component.css']
+	selector: 'app-import',
+	templateUrl: './import.component.html',
+	styleUrls: ['./import.component.css']
 })
 export class ImportComponent implements OnInit {
 
-  constructor() { }
+	defaultInputs = `userName,password,productUrl,size,proxy,proxyUserName,proxyPassword
+abc@163.com,abc123,http://www.adidas.com.cn/cg5804,42,http://61.191.41.130:80,,`;
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	import() {
+		console.log(this.defaultInputs);
+	}
 
 }
