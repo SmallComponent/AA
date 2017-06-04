12f84c54bc6b3cb31a93cd8c967d56e0f08e13d7
zhangaz1 <zhangaz1@hotmail.com>
2017-06-04 17:16:11 +0800

    use koa-router

--- src/server/index.js
+++ src/server/index.js
@@ -3,7 +3,17 @@ const Koa = require('koa');
 
 const app = new Koa();
 
-app.use(koaStatic('./static'));
+var router = require('koa-router')();
+
+router.get('/hi', function(ctx, next) {
+	ctx.body = 'Hello World!';
+	next();
+});
+
+app
+	.use(router.routes())
+	.use(router.allowedMethods())
+	.use(koaStatic('./static'));
 
 app.listen(3000);
 console.log('listening on port 3000');
