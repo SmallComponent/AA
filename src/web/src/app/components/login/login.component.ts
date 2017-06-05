83778f57c9d1095e5653aa5f76ac5b8e718cabd9
zhangaz1 <zhangaz1@hotmail.com>
2017-06-05 20:32:12 +0800

    add login and user

--- src/web/src/app/components/login/login.component.ts
+++ src/web/src/app/components/login/login.component.ts
@@ -1,15 +1,47 @@
 import { Component, OnInit } from '@angular/core';
 
+import User from './../../models/user';
+
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    user: User = {};
 
-  ngOnInit() {
-  }
+	constructor() { }
+
+	ngOnInit() {
+	}
+
+	login() {
+		if (!this.validate(true)) {
+			return;
+		}
+
+		alert('login:' + this.user.name);
+    }
+
+    register() {
+		if (!this.validate(true)) {
+			return;
+		}
+
+        alert('register:' + this.user.name);
+	}
+
+    validate(showTip: boolean = false): boolean {
+        let user = this.user;
+        let result = user.name &&
+			user.password;
+
+		if (!result && showTip) {
+			alert('name and password is required');
+		}
+
+        return result;
+    }
 
 }
