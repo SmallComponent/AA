f2d58d55beb706e2662b38b16aacc56918272894
zhangaz1 <zhangaz1@hotmail.com>
2017-06-07 20:36:47 +0800

    update test data

--- src/client/cli.js
+++ src/client/cli.js
@@ -4,16 +4,17 @@ const runMulti = require('./core/runMulti');;
 
 let context = {
 	instanceConfigs: [{
-		id: 1,
-		userName: 'abc@163.com',
-		password: 'abc123',
-		productUrl: 'http://www.adidas.com.cn/cg5804',
-		size: 42,
-		proxy: 'http://61.191.41.130:80',
-		proxyUserName: '',
-		proxyPassword: '',
-		status: 'xxx',
-	}, {
+			id: 1,
+			userName: 'abc@163.com',
+			password: 'abc123',
+			productUrl: 'http://www.adidas.com.cn/cg5804',
+			size: 42,
+			proxy: '', //'http://61.191.41.130:80',
+			proxyUserName: '',
+			proxyPassword: '',
+			status: 'xxx',
+		},
+		/*{
 		id: 2,
 		userName: 'abcd@163.com',
 		password: 'abcd163',
@@ -22,7 +23,8 @@ let context = {
 		proxy: 'http://61.191.41.130:80',
 		proxyUserName: null,
 		proxyPassword: null,
-	}, ],
+	},*/
+	],
 };
 
 runMulti.run(context);
