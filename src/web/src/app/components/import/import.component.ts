91c1e70d4000fbb0a510298e20fc1ead791122af
zhangaz1 <zhangaz1@hotmail.com>
2017-06-08 08:57:35 +0800

    complete validate

--- src/web/src/app/components/import/import.component.ts
+++ src/web/src/app/components/import/import.component.ts
@@ -1,9 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import csvjson from './../../../js/libs/csvjson/index';
-import $ from './../../../js/libs/jquery/jquery';
-import initGeetest from './../../../js/libs/gt/gt';
-import { run } from './../../../js/run';
 
 import Config  from './../../models/config';
 import Context from './../../models/context';
@@ -28,9 +25,7 @@ ${example}`;
 
 	constructor(
 		private contextService: ContextService,
-	) {
-		this.initValidateHandler();
-	}
+	) { }
 
 	ngOnInit() {
 		this.contextService.getContext()
@@ -42,44 +37,7 @@ ${example}`;
 			});
 	}
 
-	ngOnDestroy() {
-		const ipcRenderer = electron.ipcRenderer;
-
-		ipcRenderer.removeAllListeners('taskResult');
-	}
-
-	initValidateHandler() {
-		var self = this;
-		const ipcRenderer = electron.ipcRenderer;
-
-		ipcRenderer.on('taskResult', (event, data) => {
-			console.log('taskResult:', data);
-			let result = data.result;
-			self.captchaData = JSON.stringify(result);
-
-			initGeetest({
-				gt: result.gt,
-				challenge: result.challenge,
-				offline: !result.success,
-				new_captcha: result.new_captcha,
-
-				product: 'float',
-				width: '300px'
-			}, function listenCaptcha(captchaObj) {
-				captchaObj.appendTo('#captcha');
-				captchaObj.onReady(function() {
-					$('#wait').hide();
-				});
-				captchaObj.onSuccess(function() {
-					var validateData = captchaObj.getValidate();
-					self.context.instanceConfigs[0].validateData = validateData;
-					var dataString = JSON.stringify(validateData, null, 4);
-					$('#captchaValidateData').val(dataString);
-				});
-			});
-		});
-	}
-
+	ngOnDestroy() { }
 
 	import() {
 		try {
@@ -96,13 +54,6 @@ ${example}`;
 			console.error(error);
 			alert('请检查csv格式，若值中有“，”需要使用双引号');
 		}
-	}
-
-	prepareValidate() {
-		run({
-			command: 'getValidate',
-			context: {},
-		});
 	}
 
 	toCsv(obj) {
--- src/web/src/app/components/validate/validate.component.ts
+++ src/web/src/app/components/validate/validate.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import $ from './../../../js/libs/jquery/jquery';
+import initGeetest from './../../../js/libs/gt/gt';
+import { run } from './../../../js/run';
+
+import Config  from './../../models/config';
+import Context from './../../models/context';
+import {ContextService} from './../../services/context.service';
 
 @Component({
-  selector: 'app-validate',
-  templateUrl: './validate.component.html',
-  styleUrls: ['./validate.component.css']
+	selector: 'app-validate',
+	templateUrl: './validate.component.html',
+	styleUrls: ['./validate.component.css']
 })
-export class ValidateComponent implements OnInit {
+export class ValidateComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+	constructor(
+        private contextService: ContextService,
+    ) { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+		this.initValidateHandler();
+
+		const prepareValidate = this.prepareValidate;
+        this.contextService.getContext()
+            .subscribe(context => {
+                this.context = context;
+				context.instanceConfigs.forEach(prepareValidate);
+            });
+	}
+
+	ngOnDestroy() {
+		const ipcRenderer = electron.ipcRenderer;
+
+		ipcRenderer.removeAllListeners('taskResult');
+	}
+
+    initValidateHandler() {
+		var self = this;
+		const ipcRenderer = electron.ipcRenderer;
+
+		ipcRenderer.on('taskResult', (event, data) => {
+			console.log('taskResult:', data);
+			let result = data.result;
+			self.captchaData = JSON.stringify(result);
+
+			initGeetest({
+				gt: result.gt,
+				challenge: result.challenge,
+				offline: !result.success,
+				new_captcha: result.new_captcha,
+
+				product: 'float',
+				width: '300px'
+			}, function listenCaptcha(captchaObj) {
+				captchaObj.appendTo('#captcha');
+				captchaObj.onReady(function() {
+					$('#wait').hide();
+				});
+				captchaObj.onSuccess(function() {
+					var validateData = captchaObj.getValidate();
+					// self.context.instanceConfigs[0].validateData = validateData;
+					var dataString = JSON.stringify(validateData, null, 4);
+					$('#captchaValidateData').val(dataString);
+				});
+			});
+		});
+	}
+
+	prepareValidate(config, index) {
+		run({
+			command: 'getValidate',
+			context: {
+                id: config.id,
+                index: index,
+            },
+		});
+	}
 
 }
