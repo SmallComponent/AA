f07cf53b4f37ed0731cc8e0928f1751a1aa7735b
576566385@qq.com <576566385@qq.com>
2017-06-17 17:22:10 +0800

    koa static  path use relevant with code folder

--- src/server/index.js
+++ src/server/index.js
@@ -6,6 +6,8 @@ const app = new Koa();
 
 const router = require('./router');
 
+const path = require('path');
+
 const mongoOption = {
 	// uri: 'mongodb://admin:123456@localhost:27017/test', //or url
 	uri: 'mongodb://localhost:27017/aa',
@@ -18,7 +20,7 @@ app
 	.use(koaMongo(mongoOption))
 	.use(router.routes())
 	.use(router.allowedMethods())
-	.use(koaStatic('./static'));
+	.use(koaStatic(path.join(__dirname, './static')));
 
 app.listen(3000);
 console.log('listening on port 3000');
