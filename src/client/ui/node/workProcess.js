4d29302f69818e43c4e5a705b3e9d3b55c6833b6
576566385@qq.com <576566385@qq.com>
2017-06-17 18:23:31 +0800

    adust path of node.exe

--- src/client/ui/node/workProcess.js
+++ src/client/ui/node/workProcess.js
@@ -1,3 +1,27 @@
 const exec = require('child_process').exec;
+const path = require('path');
+const fs = require('fs');
 
-module.exports = exec('node ./../core/index.js');
+module.exports = startWorkProcess();
+
+function startWorkProcess(){
+    let exePath = getNodeFullpath();
+    let params = getIndexFileFullpath();
+
+    let fullCommand = exePath + ' ' + params;
+    console.log('create work process use:',fullCommand);
+
+    return exec(fullCommand);
+}
+
+function getNodeFullpath(){
+    let nodePath = path.join(__dirname,'./../../../node/node.exe');
+    if(fs.existsSync(nodePath)){
+        return nodePath;
+    }
+    return 'node';
+}
+
+function getIndexFileFullpath(){
+    return path.join(__dirname,'./../../core/index.js');
+}
